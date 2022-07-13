559c3eb811bbe12bd6612700a9e9cb01d28f6a8f
Arad Radfar <aradradfar@gmail.com>
2022-07-13 20:26:37 +0430

    content views added

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Arad Radfar on 7/13/22.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
